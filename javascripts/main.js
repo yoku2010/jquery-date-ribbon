1844f10cf9dffe21bc70de3f18a2e317c4fb680c
yoku2010 <yoku2010@gmail.com>
2012-09-13 03:05:54 +0530

    jQuery Date Ribbon Page Dcumentation for Version 1.0

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
-console.log('This would be the main JS file.');
+$(function(){
+	$("#ribbon1").dateRibbon({
+		title: "GitHub",
+		className: "ribbon",
+		href:"https://github.com/yoku2010/jQueryDateRibbon",
+		target:"_blank",
+		date:new Date()
+	});
+	$("#ribbon2").dateRibbon({
+		titleFormat:"days",
+		className: "ribbon",
+		href:"https://github.com/yoku2010/jQueryDateRibbon",
+		target:"_blank",
+		date:new Date(2012,8,12)
+	});
+});
